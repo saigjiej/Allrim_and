fbb139daac232cc1ab713777fe189b50dda5c5bf
SukMin Lee <s2019s16@e-mirim.hs.kr>
2021-04-17 15:59:06 +0900

    전체 커뮤니티, 로그인 화면 컴포넌트 구현

---
+++ app/src/main/java/com/example/allrim/CommunityMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.allrim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommunityMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_main);
+    }
+}
