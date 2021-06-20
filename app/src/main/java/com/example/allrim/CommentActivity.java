dda43517f5fe8656e1d24a67017240f97fd83db2
Lee Sukmin <s2019s16@e-mirim.hs.kr>
2021-06-20 22:30:45 +0900

    댓글 컴포넌트(CommentActivity) 추가

---
+++ app/src/main/java/com/example/allrim/CommentActivity.java
@@ -0,0 +1,140 @@
+package com.example.allrim;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CommentActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth ;
+    private DrawerLayout mDrawerLayout;
+
+    private TextView tv_nickname; // 닉네임 text
+    private ImageView iv_profile; // 이미지 뷰
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        navigationView.getMenu().getItem(4).setChecked(true);
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            mDrawerLayout.closeDrawers();
+
+            int id = menuItem.getItemId();
+            if(!menuItem.isChecked()){
+                Intent intent;
+                switch (id) {
+                    case R.id.navigation_item_info:
+                        intent = new Intent(this, MyPageActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                    case R.id.navigation_item_writing:
+                        intent = new Intent(this, MyWritingActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                    case R.id.navigation_item_schedule:
+                        intent = new Intent(this, ScheduleActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                    case R.id.navigation_item_meal:
+                        intent = new Intent(this, MealActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                    case R.id.navigation_item_lost:
+                        intent = new Intent(this, LostActivity.class);
+                        finish();
+                        startActivity(intent);
+                        break;
+                    case R.id.navigation_item_set:
+                        Toast.makeText(CommentActivity.this, "업데이트 예정", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+            return false;
+        });
+
+        String nickName = mAuth.getCurrentUser().getDisplayName(); // MainActivity로 부터 닉네임 전달받음
+        Uri photoUrl = mAuth.getCurrentUser().getPhotoUrl(); // MainActivity로 부터 프로필사진 Url 전달받음
+
+        iv_profile = headerView.findViewById(R.id.img_userImage);
+        Glide.with(this).load(photoUrl).circleCrop().into(iv_profile); // 프로필 url을 이미지 뷰에 세팅
+
+        tv_nickname = (TextView) headerView.findViewById(R.id.tv_userName);
+        tv_nickname.setText(nickName); // 닉네임 text를 텍스트 뷰에 세팅
+
+        headerView.findViewById(R.id.bt_logout).setOnClickListener(onClickListener);
+    }
+
+    // 버튼 클릭 부분
+    View.OnClickListener onClickListener = v -> {
+        switch(v.getId()){
+            case R.id.bt_logout:
+                signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                break;
+        }
+    };
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        GoogleSignInClient googleApiClient = GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build());
+        googleApiClient.signOut();
+    }
+}
